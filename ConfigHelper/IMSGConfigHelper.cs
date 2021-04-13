2f5926501ae5caf6f73fbe1af9dd6e8a9a258fe1
George <gnthackray@gmail.com>
2021-04-13 22:31:25 +0100

    config helper

---
+++ ConfigHelper/IMSGConfigHelper.cs
@@ -0,0 +1,30 @@
+﻿namespace ConfigHelper
+{
+    public interface IMSGConfigHelper
+    {
+        string TestClientUrl { get; set; }
+        string AuthServerUrl { get; set; }
+        string MSGApiGenUrl { get; set; }
+
+        string MSGGenDB01 { get; set; }
+
+        string MSGGenDB01Local { get; set; }
+
+        string DNA_Match_File_FileName { get; set; }
+
+        string DNA_Match_File_Path { get; set; }
+
+        bool DNA_Match_File_IsEncrypted { get; set; }
+
+        string FTMConString { get; set; }
+
+        string CacheData_FileName { get; set; }
+
+        string CacheData_Path { get; set; }
+
+        bool CacheData_IsEncrypted { get; set; }
+
+    }
+
+
+}
